fe045efec448f659a34d12b9ddc0f6798ea693e6
alan-konhevalic <alan.patrick.konhevalic@gmail.com>
2026-02-22 15:30:10 -0300

    alteracoes na main para testes

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -1,13 +1,27 @@
 from model.restaurante import Restaurante
+from model.cardapio.bebida import Bebida
+from model.cardapio.prato import Prato
 
 kandoo = Restaurante("Kandoo", "Sushi")
 madalosso = Restaurante("Madalosso", "Buffet")
 perez = Restaurante("Perez", "Pizza")
 
 kandoo.alternar_estado()
+kandoo.receber_avaliacao('Alan', 10)
+
+bebida = Bebida('Suco', 9.90, "Grande")
+bebida.aplicar_desconto()
+prato = Prato('Sfiha', 3.99, 'Carne')
+prato.aplicar_desconto()
+
+kandoo.adicionar_no_cardapio(bebida)
+kandoo.adicionar_no_cardapio(prato)
 
 def main():
-    Restaurante.listar_restaurantes()
+    # Restaurante.listar_restaurantes()
+    kandoo.exibir_cardapio
+
+
 
 if __name__ == '__main__':
     main()
